df437e5c15afb5c4c6767c677a7960b5287cd79d
Connor Dixon <connorddixon@gmail.com>
2024-06-11 02:26:47 +0000

    added things to gitignore

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,15 +1,15 @@
-import mysql.connector
-import os
+# import mysql.connector
+# import os
 
-config = {
-    "user": "cnrdxn",
-    "password": os.environ["mys"],
-    "host": "127.0.0.1",
-    "database": "butler"
-}
+# config = {
+#     "user": "cnrdxn",
+#     "password": os.environ["mys"],
+#     "host": "127.0.0.1",
+#     "database": "butler"
+# }
 
-try:
-    main_connection = mysql.connector.connect(**config)
-    print("successfully connected to server")
-except Exception as e:
-    print(f"nope: {e}")
+# try:
+#     main_connection = mysql.connector.connect(**config)
+#     print("successfully connected to server")
+# except Exception as e:
+#     print(f"nope: {e}")
